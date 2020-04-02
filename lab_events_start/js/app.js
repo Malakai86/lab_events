d327671eb2f006e21941f4135f252fc536f297e4
mike <>
2020-04-02 16:00:59 +0100

    title autho and category adding to page

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -2,24 +2,21 @@
 const handleFormSubmit = function (event) {
   event.preventDefault();
 
-  const resultParagrah = document.querySelector('#reading-list');
+  const readingList = document.querySelector('#reading-list');
 
-  const title = event.target.title.value;
-  const author = event.target.author.value;
+  const titleField = event.target.title.value;
+  const authorField = event.target.author.value;
+  const category = event.target.category.value;
 
-  resultParagraph.textContent = `${title}`;
-  resultParagraph.textContent = `${author}`;
+  const newListItem = document.createElement('li');
+  newListItem.textContent = `${titleField}, ${authorField}, ${category}`
+  readingList.appendChild(newListItem);
 }
 
-const handleSelectChange = function (event) {
-  const resultParagrah = document.querySelector('#reading-list');
-  resultParagrah.textContent = event.target.value;
-}
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
 
-  const select = document.querySelector('#category');
-  select.addEventListener('change', handleSelectChange);
 });
