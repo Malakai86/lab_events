df71d4c50fd3bda67c38a9100cd079b4d9915512
mike <>
2020-04-02 15:37:00 +0100

    book added to category on page

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,25 @@
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  const resultParagrah = document.querySelector('#reading-list');
+
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+
+  resultParagraph.textContent = `${title}`;
+  resultParagraph.textContent = `${author}`;
+}
+
+const handleSelectChange = function (event) {
+  const resultParagrah = document.querySelector('#reading-list');
+  resultParagrah.textContent = event.target.value;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  const select = document.querySelector('#category');
+  select.addEventListener('change', handleSelectChange);
+});
